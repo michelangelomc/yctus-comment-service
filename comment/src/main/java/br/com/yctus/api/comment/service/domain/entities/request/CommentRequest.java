d7ead8d1650a725eaa0ee13516cfd0c1a1cb536e
michelangelomc <mickelchaves@hotmail.com>
2026-02-15 13:38:44 -0300

    criando e configurando os endPoints para criar e consultar o comentário - v2

--- comment/src/main/java/br/com/yctus/api/comment/service/domain/entities/request/CommentRequest.java
+++ comment/src/main/java/br/com/yctus/api/comment/service/domain/entities/request/CommentRequest.java
@@ -2,15 +2,22 @@ package br.com.yctus.api.comment.service.domain.entities.request;
 
 import br.com.yctus.api.comment.service.infraestructure.configurations.IdGenerator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.UUID;
 
 @Data
 public class CommentRequest {
+
     @JsonIgnore
     public UUID postId = IdGenerator.generateID();
 
+    @NotNull(message = "Preenchimento Obrigatório")
     public String author;
+
+    @NotNull(message = "Preenchimento Obrigatório")
+    @Size(min = 5, max = 100, message = "O tamanho deve ser entre 5 e 100 caracteres.")
     public String comments;
 }
